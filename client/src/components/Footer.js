0b38a9d4450456cc2ebc9f09ed38df0a078f517b
Muhammad-Fahrezi <fahrezi@mhs.unsyiah.ac.id>
2022-06-18 17:08:23 +0700

    Update Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,25 +8,25 @@ import yt from "../assets/footer/youtube.svg"
 
 
 // import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
-import bootstrap from 'bootstrap'
+// import bootstrap from 'bootstrap'
 
 export default function Main () {
     return (
         <>              
             {/* Footer start */}
-            <div class="card text-center mt-5 pt-4" style={{backgroundColor: '#343A40'}}>
-                {/* <div class="card-header">
-                    Featured
-                </div> */}
+            <div class="card mt-5 pt-4" style={{backgroundColor: '#343A40'}}>
+                <div class="card-header d-flex justify-content-between text-light" style={{backgroundColor:'transparent'}}>
+                    <div className="text-start fs-2 ps-5">
+                        Ruang Event
+                    </div>
+                    <div className="text-end pt-3" style={{paddingRight: '165px'}}>
+                        Hubungi Kami
+                    </div>
+                </div>
+
                 <div className="container p-0">
-                    <div class="card-body d-flex flex-column flex-start flex-lg-row justify-content-lg-between">
-                        {/* <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a> */}
-                        <div style={{color: '#fff'}} className="text-start fs-2">
-                            Ruang Event
-                        </div>
-                        <div className="d-flex flex-row gap-5 mt-3 mt-lg-0 py-4">
+                    <div class="card-body d-flex flex-column flex-start flex-row-reverse justify-content-lg-between">
+                        <div className="d-flex flex-row-reverse gap-5 mt-3 mt-lg-0 py-4">
                             <div style={{backgroundColor: '#4DD4AC'}} className="d-flex align-items-center p-1 rounded">   
                                 <img src={ig} width="20px" className="img-fluid"/>
                             </div>
@@ -42,11 +42,16 @@ export default function Main () {
                         </div>
                     </div>
                 </div>
-                <div class="card-footer text-muted" style={{backgroundColor: '#4DD4AC'}}>
+
+                <div class="card-footer d-flex justify-content-evenly" style={{backgroundColor: '#4DD4AC'}}>
+                    <div className="text-muted">
                         Copyright © 2022-PRESENT, Ruang Event Teams
+                    </div>
+                    <div >
+                        Kebijakan Privasi | Syarat dan Ketentuan
+                    </div>
                 </div>
             </div>
-
             {/* Footer end  */}
         </>
     )
