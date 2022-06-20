52b1e2d552a957d613753a5220ca6a846536cdc1
fachriansyahmni <fachriansyahmni@gmail.com>
2022-06-20 22:54:25 +0700

    feat: add login unit testing

--- server/route/router.go
+++ server/route/router.go
@@ -50,6 +50,10 @@ func Newrouter(authentication *handler.AuthHandler, userHandler *handler.UserHan
 	return router
 }
 
+func (a *Router) Handler() *gin.Engine {
+	return a.route
+}
+
 func (a *Router) Run(port string) {
 	a.route.Run(port)
 }
