fe27dfb5c9e396c45e853e463ad84cd76c98d243
fachriansyahmni <fachriansyahmni@gmail.com>
2022-06-25 20:05:26 +0700

    fix: update event service api

--- server/payloads/EventRequest.go
+++ server/payloads/EventRequest.go
@@ -16,6 +16,22 @@ type EventRequest struct {
 	RegisterUrl     string `json:"register_url"`
 }
 
+type EventUpdateRequest struct {
+	ID              int64  `json:"id"`
+	Title           string `json:"title"`
+	BannerImg       string `json:"banner_img"`
+	Content         string `json:"content"`
+	CategoryID      int64  `json:"category_id"`
+	StartTimeEvent  string `json:"start_time_event"`
+	StartDateEvent  string `json:"start_date_event"`
+	Contact         string `json:"contact"`
+	Price           int64  `json:"price"`
+	TypeEventID     int64  `json:"type_event_id"`
+	ModelID         int64  `json:"model_id"`
+	LocationDetails string `json:"location_details"`
+	RegisterUrl     string `json:"register_url"`
+}
+
 type EventIdRequest struct {
 	ID int64 `json:"id"`
 }
